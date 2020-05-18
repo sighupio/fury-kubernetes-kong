0abeebf984655b06d66a9d7dbb3efa8f77c078ef
Samuele Chiocca <samuele@sighup.io>
2020-05-18 13:17:45 +0200

    Moving from Deployment to Daemonset

--- katalog/tests/kong.sh
+++ katalog/tests/kong.sh
@@ -49,7 +49,6 @@ set -o pipefail
     for state in $status; do
       if [ "${state}" != "Running" ]; then return 1; fi
     done
-  }
   loop_it test 30 2
   status=${loop_it_result}
   [[ "$status" -eq 0 ]]
@@ -58,7 +57,7 @@ set -o pipefail
 @test "Check that Petstore example app is working via GET /pet/1 route" {
   info
   test() {
-    http_code=$(curl "http://${INSTANCE_IP}:80/pet/1" -s -o /dev/null -w "%{http_code}")
+    http_code=$(curl "http://${INSTANCE_IP}/pet/1" -s -o /dev/null -w "%{http_code}")
     if [ "${http_code}" -ne "200" ]; then return 1; fi
   }
   run test
@@ -68,7 +67,7 @@ set -o pipefail
 @test "Check that Petstore example app denying POST on /pet/1 route" {
   info
   test() {
-    http_code=$(echo '{"stuff": 1}' | curl -d @- "http://${INSTANCE_IP}:80/pet/1" --header "Content-Type:application/json" -s -o /dev/null -w "%{http_code}")
+    http_code=$(echo '{"stuff": 1}' | curl -d @- "http://${INSTANCE_IP}/pet/1" --header "Content-Type:application/json" -s -o /dev/null -w "%{http_code}")
     if [ "${http_code}" -ne "404" ]; then return 1; fi
   }
   run test
@@ -78,7 +77,7 @@ set -o pipefail
 @test "Testing Petstore example app rate limiting /pet/1 route" {
   info
   test() {
-    for i in {1..5}; do http_code=$(curl "http://${INSTANCE_IP}:80/pet/1" -s -o /dev/null -w "%{http_code}"); done
+    for i in {1..5}; do http_code=$(curl "http://${INSTANCE_IP}/pet/1" -s -o /dev/null -w "%{http_code}"); done
     if [ "${http_code}" -ne "429" ]; then return 1; fi
   }
   run test
@@ -89,7 +88,7 @@ set -o pipefail
 @test "Testing Petstore example app basic auth /user route" {
   info
   test() {
-    http_code=$(echo '{"id": 1,"username": "user","firstName": "user","lastName": "user","email": "user@user.tld","password": "user","phone": "1234","userStatus": 0 }' | curl -d @- http://${INSTANCE_IP}:80/user  -u user:123456 --header "Content-Type:application/json" -s -o /dev/null -w "%{http_code}")
+    http_code=$(echo '{"id": 1,"username": "user","firstName": "user","lastName": "user","email": "user@user.tld","password": "user","phone": "1234","userStatus": 0 }' | curl -d @- http://${INSTANCE_IP}/user  -u user:123456 --header "Content-Type:application/json" -s -o /dev/null -w "%{http_code}")
     if [ "${http_code}" -ne "200" ]; then return 1; fi
   }
   run test
@@ -100,7 +99,7 @@ set -o pipefail
 @test "Check that Petstore example app is working via GET /store/inventory route and host awesome-kong.io" {
   info
   test() {
-    http_code=$(curl "http://${INSTANCE_IP}:80/store/inventory" -H 'host: awesome-kong.io' -s -o /dev/null -w "%{http_code}")
+    http_code=$(curl "http://${INSTANCE_IP}/store/inventory" -H 'host: awesome-kong.io' -s -o /dev/null -w "%{http_code}")
     if [ "${http_code}" -ne "200" ]; then return 1; fi
   }
   run test
